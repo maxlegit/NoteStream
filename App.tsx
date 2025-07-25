2561e2aca5eeecff3f67e969442da9131a70299a
maxlegit <71774170+maxlegit@users.noreply.github.com>
2025-07-25 14:34:52 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to NoteStream 🎉</h1>
+      <p>This is your React app running with Vite + GitHub Pages!</p>
+    </div>
+  );
+}
+
+export default App;
